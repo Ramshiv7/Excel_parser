ae58c93f7bea8fe290f32808d5f76911bd23fb9e
Ramshiv7 <67915300+Ramshiv7@users.noreply.github.com>
2021-03-26 10:08:07 +0530

    Update Excel_par.py

--- Excel_par.py
+++ Excel_par.py
@@ -1,21 +1,29 @@
 import os, pandas, zipfile, sys
 
 def csv_parser(path):
+
+    #Changing the working directory
     os.chdir(path)
 
+    #Traversing through the CSV Files
     for file_name in os.listdir(path):
-        if file_name.endswith('.csv') and file_name != "Combined":
+        if file_name.endswith('.csv') and file_name != "Combined" or file_name != "Combined_temp":
             full_name = os.path.join(path,file_name)
             df = pandas.read_csv(file_name)
             de_dupe_ip = df.drop_duplicates(subset=['Source IP'])
-            de_dupe_ip.sort_values(by=['Source IP'], kind='quicksort', ignore_index=False)
             envname = os.path.splitext(file_name)[0].strip('0123456789')
             dt = pandas.DataFrame(de_dupe_ip, columns= ['Source IP'])
             dt['Environment']= envname
-            dt.to_csv(f'{path}\Combined.csv', index=False, header=False, mode='a')
+            dt.to_csv(f'{path}\Combined_temp.csv', index=False, header=False, mode='a')
 
         else:
             pass
+
+    #Sorting the Combined CSV file
+    final = pandas.read_csv(f'{path}\Combined_temp.csv')
+    final.columns = ['Source IP', 'ENV']
+    final_data = final.sort_values(by=['Source IP'], ascending=True)
+    final_data.to_csv(f'{path}\Combined.csv', index=False)
 
 
 if __name__ == '__main__':
